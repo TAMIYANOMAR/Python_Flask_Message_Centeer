c8ec6eb3b577686398001e46ecfe7f7d4bbbc092
ATtami YYNa <tami19996@gmail.com>
2022-06-17 19:15:26 +0900

    add logout function

--- webapp.py
+++ webapp.py
@@ -21,23 +21,32 @@ def CheckLogin(ipadress):
 def GetUserNameFromIp(ipaderess):
     return Login_users[ipaderess]
 
+def LogoutFromUser(ipadress):
+    try:
+        Login_users.pop(ipadress)
+    except:
+        return
+    return
+
 def CheckSignin(username,password,ipadress):
-    print("checking:::")
-    stmt = 'SELECT passWord FROM users WHERE name = %s'
-    cur = connector_toDB.cursor()
-    param = (username,)
-    cur.execute(stmt,param)
-    truePass = cur.fetchall()
-    cur.close()
-    if password == truePass[0][0]:
-        Login_users.setdefault(ipadress,username)
-        return True
-    return False
+    try:
+        stmt = 'SELECT passWord FROM users WHERE name = %s'
+        cur = connector_toDB.cursor()
+        param = (username,)
+        cur.execute(stmt,param)
+        truePass = cur.fetchall()
+        cur.close()
+        if password == truePass[0][0]:
+            Login_users.setdefault(ipadress,username)
+            return True
+        return False
+    except:
+        return False
 
 @app.route('/',methods =['GET','POST'])
 def main():
     if flask.request.method == "GET":
-        props = {'title': 'Index', 'msg': 'Welcome to Index Page.'}
+        props = {'title': 'Index', 'msg': 'Welcome to Message Center'}
         return render_template('index.html', props=props)
     if flask.request.method == 'POST':
         username = flask.request.form['username']
@@ -62,6 +71,12 @@ def signup():
     if flask.request.method == "GET":
         return render_template('signup.html',props = "sign up")
 
+@app.route('/logout',methods = ["GET"])
+def logout():
+    userip = flask.request.remote_addr
+    LogoutFromUser(userip)
+    return redirect('/')
+
 @app.route('/message/home', methods = ["GET"])
 def msghome():
     if CheckLogin(flask.request.remote_addr) == False:
@@ -74,7 +89,7 @@ def msghome():
 @app.route("/message/post", methods=["POST"])
 def reciece_msg():
     if CheckLogin(flask.request.remote_addr) == False:
-        redirect('/')
+        return redirect('/')
     username = GetUserNameFromIp(flask.request.remote_addr)
     content = flask.request.form["content"]
     postTo = flask.request.form["postTo"]
@@ -91,13 +106,12 @@ def get_msg():
         redirect('/')
     username = GetUserNameFromIp(flask.request.remote_addr)
     postFrom = flask.request.form["postFrom"]
-    stmt = 'SELECT content FROM messages WHERE postFrom = %s AND postTo = %s'
+    stmt = 'SELECT * FROM messages WHERE (postFrom = %s AND postTo = %s) OR (postFrom = %s AND postTo = %s)'
     cur = connector_toDB.cursor()
-    param = (postFrom,username)
+    param = (postFrom,username,username,postFrom)
     cur.execute(stmt,param)
     Messages = cur.fetchall()
     cur.close()
-    print(Messages)
     return render_template('resultGet.html', postFrom = postFrom,MessageContents = Messages)
 
 
